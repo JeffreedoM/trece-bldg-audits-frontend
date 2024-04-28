0550755a3d230eed82bd631f5326cac3ded723fc
Jeffrey Nunez <jeffreynunez029@gmail.com>
2024-04-28 16:34:35 +0800

    balik ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,6 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "react/jsx-no-target-blank": "off",
-    "@typescript-eslint/ban-ts-ignore": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
